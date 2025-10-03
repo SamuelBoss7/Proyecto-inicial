6e5abf474fec64fb77e88525a596e33fea6ac3e2
SamuelBoss7 <abnersamuelbrudasca5@gmail.com>
2025-10-03 17:33:09 +0200

    he creado 6 carpetas

---
+++ DAM/Proyecto_inicial.java
@@ -0,0 +1,18 @@
+package DAM;
+
+public class Proyecto_inicial {
+
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("¡Hola Mundo! Este es mi primer programa Java.");
+        System.out.print("Estamos aprendiendo a programar");
+        System.out.println(" en el módulo DAM.");
+        System.out.println("Yo tengo ahora mismo la edad de 18 años que los he cumplido el día 6 de septiembre de este año! ");
+        System.out.println("Por cierto no sé si lo dije antes, pero me llamo Samuel");
+        System.out.print("Y ahora mismo me estoy sacando el carnet, hace ya unos dias ");
+        System.out.print("que he empezado con el practico, hoy me vuelve a tocar ");
+    }}
+    
+
